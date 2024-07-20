315056e106e939d614f9070d96d3e1049036eb4e
jeus1998 <zeus20240228@gmail.com>
2024-07-20 19:19:08 +0900

    자바 ORM 표준 JPA 프로그래밍 - 기본편 4-5 엔티티 매핑 - 실전 예제1 - 요구사항 분석과 기본 매핑

---
+++ JPA1/jpashop/src/main/java/jpabook/jpashop/JpaMain.java
@@ -0,0 +1,32 @@
+package jpabook.jpashop;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Persistence;
+
+public class JpaMain {
+    public static void main(String[] args) {
+        // EntityManagerFactory 생성 (persistenceUnitName 필요)
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        // EntityManger 생성 => 항상 어떤 데이터베이스에 쿼리를 날릴 때(트랜잭션 단위) EntityManager 생성한다.
+        EntityManager em = emf.createEntityManager();
+        // 트랜잭션 획득, 시작
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+
+            tx.commit();
+        }
+        catch (Exception e){
+            tx.rollback();
+        }
+        finally {
+            em.close();
+        }
+
+        // 애플리케이션 종료시점에 EntityManagerFactory close
+        emf.close();
+    }
+}
