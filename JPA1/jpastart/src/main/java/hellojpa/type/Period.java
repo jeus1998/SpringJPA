9c42a44756b4e6377062f7779cfbe7733453b74a
jeus1998 <zeus20240228@gmail.com>
2024-07-26 23:08:55 +0900

    자바 ORM 표준 JPA 프로그래밍 - 기본편 9-2 값 타입 - 임베디드 타입

---
+++ JPA1/jpastart/src/main/java/hellojpa/type/Period.java
@@ -0,0 +1,11 @@
+package hellojpa.type;
+
+import jakarta.persistence.Embeddable;
+
+import java.time.LocalDateTime;
+
+@Embeddable
+public class Period {
+    private LocalDateTime startDate;
+    private LocalDateTime endDate;
+}
---
+++ JPA1/jpastart/src/main/java/hellojpa/type/Test.java
@@ -0,0 +1,27 @@
+package hellojpa.type;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+/**
+ * 임베디드 타입 테스트 용
+ */
+@Entity
+public class Test {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String name;
+    @Embedded
+    private Period period;
+    @Embedded
+    private Address address;
+    @Embedded
+    @AttributeOverrides({
+            @AttributeOverride(name = "city", column = @Column(name = "WORK_CITY")),
+            @AttributeOverride(name = "street", column = @Column(name = "WORK_STREET")),
+            @AttributeOverride(name = "zipcode", column = @Column(name = "WORK_ZIP_CODE"))
+    })
+    private Address workAddress;
+}
