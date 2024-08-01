ee06dc4861500ad85274e00687957285f50fc0b4
jeus1998 <zeus20240228@gmail.com>
2024-08-01 15:54:23 +0900

    스프링 부트와 JPA 활용1 - 웹 애플리케이션 개발 1-1 프로젝트 환경설정 - 프로젝트 생성

--- JPA2/jpashop/src/main/java/jpabook/jpashop/JpashopApplication.java
+++ JPA2/jpashop/src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -8,6 +8,9 @@ public class JpashopApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JpashopApplication.class, args);
+		Hello hello = new Hello();
+		hello.setData("data");
+		System.out.println("hello = " + hello);
 	}
 
 }
