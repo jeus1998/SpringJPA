99a9754e4ca092197b152b42ed1c7c2df05b40ea
jeus1998 <zeus20240228@gmail.com>
2024-08-05 15:45:44 +0900

    스프링 부트와 JPA 활용1 - 웹 애플리케이션 개발 7-3 웹 계층 개발 - 회원 목록 조회

--- JPA2/jpashop/src/main/java/jpabook/jpashop/controller/MemberController.java
+++ JPA2/jpashop/src/main/java/jpabook/jpashop/controller/MemberController.java
@@ -5,15 +5,17 @@ import jpabook.jpashop.domain.Address;
 import jpabook.jpashop.domain.Member;
 import jpabook.jpashop.service.MemberService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
+@Slf4j
 public class MemberController {
     private final MemberService memberService;
     @GetMapping("/members/new")
@@ -25,7 +27,7 @@ public class MemberController {
     public String create(@Valid @ModelAttribute MemberForm form, BindingResult result ){
 
         if(result.hasErrors()){
-            return "/members/createMemberForm";
+             return "/members/createMemberForm";
         }
 
         Address address = new Address(form.getCity(), form.getStreet(), form.getZipcode());
@@ -34,6 +36,16 @@ public class MemberController {
         member.setAddress(address);
 
         memberService.join(member);
+
         return "redirect:/";
     }
+
+    @GetMapping("/members")
+    public String list(Model model){
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
+    }
+
+
 }
