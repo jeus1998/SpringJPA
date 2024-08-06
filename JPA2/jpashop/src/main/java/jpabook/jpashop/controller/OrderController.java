d0f63139f07f28746bac1ecef1330e0ff3c70aa8
jeus1998 <zeus20240228@gmail.com>
2024-08-06 12:39:56 +0900

    스프링 부트와 JPA 활용1 - 웹 애플리케이션 개발 7-8 웹 계층 개발 - 상품 주문

---
+++ JPA2/jpashop/src/main/java/jpabook/jpashop/controller/OrderController.java
@@ -0,0 +1,79 @@
+package jpabook.jpashop.controller;
+
+import jakarta.annotation.PostConstruct;
+import jpabook.jpashop.domain.Address;
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.domain.item.Book;
+import jpabook.jpashop.domain.item.Item;
+import jpabook.jpashop.service.ItemService;
+import jpabook.jpashop.service.MemberService;
+import jpabook.jpashop.service.OrderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+public class OrderController {
+    private final OrderService orderService;
+    private final MemberService memberService;
+    private final ItemService itemService;
+
+    /**
+     * 테스트 데이터 넣기 위한 init()
+     * member 2개
+     * 상품 2개
+     */
+    @PostConstruct
+    public void init(){
+        Member member = new Member();
+        member.setName("member1");
+        member.setAddress(new Address("부산", "광안리", "1234"));
+        memberService.join(member);
+
+        Member member2 = new Member();
+        member2.setName("member2");
+        member2.setAddress(new Address("인천", "차이나타운", "2344"));
+        memberService.join(member2);
+
+        Book book = new Book();
+        book.setName("JPA");
+        book.setPrice(10000);
+        book.setStockQuantity(20);
+        book.setAuthor("김영한");
+        itemService.saveItem(book);
+
+        Book book2 = new Book();
+        book2.setName("삼국지");
+        book2.setPrice(30000);
+        book2.setStockQuantity(40);
+        book2.setAuthor("배제우");
+        itemService.saveItem(book2);
+    }
+
+    @GetMapping("/order")
+    public String createForm(Model model){
+        List<Member> members = memberService.findMembers();
+        List<Item> items = itemService.findItems();
+        model.addAttribute("members", members);
+        model.addAttribute("items", items);
+
+        return "/order/orderForm";
+    }
+
+    @PostMapping("/order")
+    public String order(@RequestParam("memberId") Long memberId,
+                        @RequestParam("itemId") Long itemId,
+                        @RequestParam("count") int count){
+
+        orderService.order(memberId, itemId, count);
+
+        return "redirect:/orders";
+    }
+
+}
