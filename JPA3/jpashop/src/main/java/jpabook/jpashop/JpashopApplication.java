a2d1375f244582683e4bdad8c4f266f0888aa72e
jeus1998 <zeus20240228@gmail.com>
2024-08-07 14:45:35 +0900

    스프링 부트와 JPA 활용2 - API 개발 & 성능 최적화 3-1 API 개발 고급 - 간단한 주문 조회 V1 - 엔티티를 직접 노출

--- JPA3/jpashop/src/main/java/jpabook/jpashop/JpashopApplication.java
+++ JPA3/jpashop/src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -1,8 +1,10 @@
 package jpabook.jpashop;
 
+import com.fasterxml.jackson.datatype.hibernate5.jakarta.Hibernate5JakartaModule;
 import jpabook.jpashop.hello.Hello;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class JpashopApplication {
@@ -12,6 +14,18 @@ public class JpashopApplication {
 		Hello hello = new Hello();
 		hello.setData("data");
 		System.out.println("hello = " + hello);
+	}
+
+	/**
+	 * 기본적으로 초기화 된 프록시 객체만 노출, 초기화 되지 않은 프록시 객체는 노출 안함
+	 * + 강제 지연 로딩 설정
+	 */
+	@Bean
+	Hibernate5JakartaModule hibernate5JakartaModule(){
+		Hibernate5JakartaModule hibernate5JakartaModule = new Hibernate5JakartaModule();
+		// 강제 지연 로딩 설정
+		// hibernate5JakartaModule.configure(Hibernate5JakartaModule.Feature.FORCE_LAZY_LOADING, true);
+		return hibernate5JakartaModule;
 	}
 
 }
