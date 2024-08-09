a094356640fc40979692d1d4d3dd07bbcd6bbe51
jeus1998 <zeus20240228@gmail.com>
2024-08-09 14:22:08 +0900

    스프링 데이터 JPA 3-1 공통 인터페이스 기능 - 순수 JPA 기반 리포지토리 만들기

--- JPA4/data-jpa/src/main/java/spring/data_jpa/repository/MemberJpaRepository.java
+++ JPA4/data-jpa/src/main/java/spring/data_jpa/repository/MemberJpaRepository.java
@@ -4,6 +4,7 @@ import jakarta.persistence.EntityManager;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 import spring.data_jpa.entity.Member;
+import java.util.*;
 
 @Repository
 @RequiredArgsConstructor
@@ -11,11 +12,23 @@ public class MemberJpaRepository {
 
     // @PersistenceContext
     private final EntityManager em;
-
     public Member save(Member member){
         em.persist(member);
         return member;
     }
+    public void delete(Member member){
+        em.remove(member);
+    }
+    public List<Member> findAll(){
+        return em.createQuery("select m from Member m", Member.class).getResultList();
+    }
+    public Optional<Member> findById(Long id){
+        return Optional.ofNullable(em.find(Member.class, id));
+    }
+    public long count(){
+        return em.createQuery("select count(m) from Member m", Long.class).getSingleResult();
+    }
+
     public Member find(Long id){
         return em.find(Member.class, id);
     }
