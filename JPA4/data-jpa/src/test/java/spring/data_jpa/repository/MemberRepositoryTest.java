0adb1e24c02cab87b0105d93428f0b234cf94a6c
jeus1998 <zeus20240228@gmail.com>
2024-08-09 14:31:25 +0900

    스프링 데이터 JPA 3-2 공통 인터페이스 기능 - 공통 인터페이스 설정

--- JPA4/data-jpa/src/test/java/spring/data_jpa/repository/MemberRepositoryTest.java
+++ JPA4/data-jpa/src/test/java/spring/data_jpa/repository/MemberRepositoryTest.java
@@ -16,6 +16,9 @@ class MemberRepositoryTest {
 
     @Test
     public void testMember(){
+
+        System.out.println("memberRepository.getClass() = " + memberRepository.getClass());
+
         Member member = new Member("memberA");
         Member savedMember = memberRepository.save(member);
 
