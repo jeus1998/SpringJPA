a8ba4f882ec014933dd88650d2518aba6922aecf
jeus1998 <zeus20240228@gmail.com>
2024-08-13 15:56:57 +0900

    스프링 데이터 JPA 5-3 확장 기능 - Web 확장 - 도메인 클래스 컨버터

---
+++ JPA4/data-jpa/src/main/java/spring/data_jpa/controller/MemberController.java
@@ -0,0 +1,34 @@
+package spring.data_jpa.controller;
+
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import spring.data_jpa.entity.Member;
+import spring.data_jpa.repository.MemberRepository;
+
+@RestController
+@RequiredArgsConstructor
+public class MemberController {
+    private final MemberRepository memberRepository;
+    @GetMapping("/members/{id}")
+    public String findMember(@PathVariable("id") Long id){
+        Member member = memberRepository.findById(id).get();
+        return member.getUsername();
+    }
+
+    /**
+     * 도메인 클래스 컨버터 사용
+     */
+    @GetMapping("/members2/{id}")
+    public String findMember2(@PathVariable("id") Member member){
+        return member.getUsername();
+    }
+
+    @PostConstruct
+    public void init(){
+        Member member = new Member("userA", 10);
+        memberRepository.save(member);
+    }
+}
