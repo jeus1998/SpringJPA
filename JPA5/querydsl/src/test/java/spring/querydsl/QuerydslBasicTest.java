ca90aa877e9157537bbf859ec29327b88aec097b
jeus1998 <zeus20240228@gmail.com>
2024-08-14 19:54:27 +0900

    Querydsl 3-13 기본 문법 - 상수, 문자 더하기

--- JPA5/querydsl/src/test/java/spring/querydsl/QuerydslBasicTest.java
+++ JPA5/querydsl/src/test/java/spring/querydsl/QuerydslBasicTest.java
@@ -3,6 +3,7 @@ package spring.querydsl;
 import com.querydsl.core.QueryResults;
 import com.querydsl.core.Tuple;
 import com.querydsl.core.types.dsl.CaseBuilder;
+import com.querydsl.core.types.dsl.Expressions;
 import com.querydsl.core.types.dsl.NumberExpression;
 import com.querydsl.jpa.JPAExpressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
@@ -491,6 +492,34 @@ public class QuerydslBasicTest {
                 .fetch();
         for (Member member : result) {
             System.out.println("member = " + member);
+        }
+    }
+
+    /**
+     * 상수
+     */
+    @Test
+    public void constant(){
+        List<Tuple> result = queryFactory
+                .select(member.username, Expressions.constant("A"))
+                .from(member)
+                .fetch();
+        for (Tuple tuple : result) {
+            System.out.println("tuple = " + tuple);
+        }
+    }
+
+    @Test
+    public void concat(){
+        //{username}_{age}
+        List<String> result = queryFactory
+                .select(member.username.concat("_").concat(member.age.stringValue()))
+                .from(member)
+                .where(member.username.eq("member1"))
+                .fetch();
+
+        for (String s : result) {
+            System.out.println("s = " + s);
         }
     }
 
