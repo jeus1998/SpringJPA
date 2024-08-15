656afe3d4c73c84348723756d79d5873f5f41923
jeus1998 <zeus20240228@gmail.com>
2024-08-15 11:18:49 +0900

    Querydsl 4-1 중급 문법 - 프로젝션 결과 반환 - 기본

--- JPA5/querydsl/src/test/java/spring/querydsl/QuerydslBasicTest.java
+++ JPA5/querydsl/src/test/java/spring/querydsl/QuerydslBasicTest.java
@@ -523,4 +523,31 @@ public class QuerydslBasicTest {
         }
     }
 
+    @Test
+    public void simpleProjection(){
+        List<String> result = queryFactory
+                .select(member.username)
+                .from(member)
+                .fetch();
+        for (String name : result) {
+            System.out.println("name = " + name);
+        }
+    }
+
+    @Test
+    public void tupleProjection(){
+        List<Tuple> result = queryFactory
+                .select(member.username, member.age)
+                .from(member)
+                .fetch();
+
+        for (Tuple tuple : result) {
+            String username = tuple.get(member.username);
+            System.out.println("username = " + username);
+            Integer age = tuple.get(member.age);
+            System.out.println("age = " + age);
+        }
+    }
+
+
 }
