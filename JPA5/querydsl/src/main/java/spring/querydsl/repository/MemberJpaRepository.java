a1b87d808d33138b890f0241207a743e1f813fee
jeus1998 <zeus20240228@gmail.com>
2024-08-15 18:48:44 +0900

    Querydsl 5-2 실무 활용 - 동적 쿼리와 성능 최적화 조회 - Builder 사용

--- JPA5/querydsl/src/main/java/spring/querydsl/repository/MemberJpaRepository.java
+++ JPA5/querydsl/src/main/java/spring/querydsl/repository/MemberJpaRepository.java
@@ -1,14 +1,21 @@
 package spring.querydsl.repository;
 
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.types.dsl.Expressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.StringUtils;
+import spring.querydsl.dto.MemberSearchCondition;
+import spring.querydsl.dto.MemberTeamDto;
+import spring.querydsl.dto.QMemberTeamDto;
 import spring.querydsl.entity.Member;
 import spring.querydsl.entity.QMember;
 
 import java.util.*;
 
 import static spring.querydsl.entity.QMember.*;
+import static spring.querydsl.entity.QTeam.*;
 
 @Repository
 public class MemberJpaRepository {
@@ -42,6 +49,30 @@ public class MemberJpaRepository {
         return queryFactory
                 .selectFrom(member)
                 .where(member.username.eq(username))
+                .fetch();
+    }
+    public List<MemberTeamDto> searchByBuilder(MemberSearchCondition condition){
+        BooleanBuilder builder = new BooleanBuilder();
+
+        if(StringUtils.hasText(condition.getUsername())){
+            builder.and(member.username.eq(condition.getUsername()));
+        }
+        if(StringUtils.hasText(condition.getTeamName())){
+            builder.and(team.name.eq(condition.getTeamName()));
+        }
+        if(condition.getAgeGoe() != null){
+            builder.and(member.age.goe(condition.getAgeGoe()));
+        }
+        if(condition.getAgeLoe() != null){
+            builder.and(member.age.loe(condition.getAgeLoe()));
+        }
+
+        return queryFactory
+                .select(new QMemberTeamDto(
+                        member.id, member.username, member.age, team.id , team.name))
+                .from(member)
+                .leftJoin(member.team, team)
+                .where(builder)
                 .fetch();
     }
 }
--- JPA5/querydsl/src/test/java/spring/querydsl/repository/MemberJpaRepositoryTest.java
+++ JPA5/querydsl/src/test/java/spring/querydsl/repository/MemberJpaRepositoryTest.java
@@ -5,7 +5,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
+import spring.querydsl.dto.MemberSearchCondition;
+import spring.querydsl.dto.MemberTeamDto;
 import spring.querydsl.entity.Member;
+import spring.querydsl.entity.Team;
 
 import java.util.List;
 
@@ -43,5 +46,35 @@ class MemberJpaRepositoryTest {
 
         List<Member> result2 = memberJpaRepository.findByUsername_Querydsl(member.getUsername());
         assertThat(result2).containsExactly(member);
+    }
+
+    @Test
+    public void searchTest(){
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        em.persist(teamA);
+        em.persist(teamB);
+
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 20, teamA);
+        Member member3 = new Member("member3", 30, teamB);
+        Member member4 = new Member("member4", 40, teamB);
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+
+        MemberSearchCondition condition = new MemberSearchCondition();
+        condition.setAgeGoe(35);
+        condition.setAgeLoe(40);
+        condition.setTeamName("teamB");
+
+        List<MemberTeamDto> result = memberJpaRepository.searchByBuilder(condition);
+        for (MemberTeamDto memberTeamDto : result) {
+            System.out.println("memberTeamDto = " + memberTeamDto);
+        }
+
+        assertThat(result).extracting("username")
+                .containsExactly("member4");
     }
 }
